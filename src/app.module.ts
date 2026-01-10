68f5ba7d93392f2c50840f4098bdbf28fed4a1ef
Felipe Pereira <fehpereira237@gmail.com>
2026-01-10 13:06:48 -0300

    feat: add environment variable validation with Zod and refactor main.ts to use ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service.js';
 import { CreateAccountController } from './controllers/create-account.controller.js';
 import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './env.js';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
   ],
